47de6790f073d625d6ff37c32ba5f42bb36d57fa
Hatranax <queen.artesia@gmail.com>
2022-09-17 21:14:30 +0300

    Q

--- homework_02/base.py
+++ homework_02/base.py
@@ -8,7 +8,7 @@ class Vehicle(ABC):
     fuel = 0
     fuel_consumption = 0
     
-    def __init__(w,f,f_c):
+    def __init__(self,w,f,f_c):
         weight = w
         fuel = f
         fuel_consumption_= f_c
