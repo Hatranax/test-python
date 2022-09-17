05bc538d635e7cb91edc40f95fa32f2dd04abcb5
Hatranax <queen.artesia@gmail.com>
2022-09-17 21:11:01 +0300

    a

--- homework_02/base.py
+++ homework_02/base.py
@@ -1,5 +1,14 @@
 from abc import ABC
+from email.errors import StartBoundaryNotFoundDefect
 
 
 class Vehicle(ABC):
-    pass
+    weight = 0
+    started = False
+    fuel = 0
+    fuel_consumption = 0
+    
+    def __init__(w,f,f_c):
+        weight = w
+        fuel = f
+        fuel_consumption_= f_c
