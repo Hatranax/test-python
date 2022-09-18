f6c3c74a7d2265dcefb8f5e5653ddd62c0147770
Hatranax <queen.artesia@gmail.com>
2022-09-18 11:26:31 +0300

    11

--- homework_02/car.py
+++ homework_02/car.py
@@ -2,3 +2,9 @@
 создайте класс `Car`, наследник `Vehicle`
 """
 from base import Vehicle
+
+class Car(Vehicle):
+    engine = None
+
+    def set_engine(self,Vengine):
+        self.engine = Vengine
--- homework_02/engine.py
+++ homework_02/engine.py
@@ -1,6 +1,9 @@
 """
 create dataclass `Engine`
 """
-class Engine:
-    volume = 0
-    pistons = 0
+class Engine():
+    volume = None
+    pistons = None
+    def __init__(self,volume=None,pistons=None):
+        self.volume = volume
+        self.pistons = pistons
